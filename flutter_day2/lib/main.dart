59c96f2ff34b8f5f013aab7b4c07b5700bee7552
MartianCoder-gitgit config --global user.name MartianCoder-git <cpanesar39@gmail.com>
2021-02-06 15:48:00 +0530

    day3 completed

--- flutter_day2/lib/main.dart
+++ flutter_day2/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_day2/home_page.dart';
+import 'package:flutter_day2/pages/login_page.dart';
+import 'pages/home_page.dart';
 void main() {
   runApp(MyApp()); 
 }
@@ -16,7 +17,18 @@ class MyApp extends StatelessWidget {
     
 
     return MaterialApp(
-      home:HomePage(),
+      //home:HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+     initialRoute: "/home",
+     routes: {
+       "/" : (context) => LoginPage(),
+       "/home":(context) => HomePage(),
+       "/login":(context) => LoginPage()
+     },
     );
   }
 }
