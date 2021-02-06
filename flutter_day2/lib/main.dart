6ba8aa59d2cccc418310893615b4d7e65d5c2483
MartianCoder-gitgit config --global user.name MartianCoder-git <cpanesar39@gmail.com>
2021-02-06 18:45:24 +0530

    Day4 completed

--- flutter_day2/lib/main.dart
+++ flutter_day2/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_day2/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(MyApp()); 
 }
@@ -19,7 +21,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //home:HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
